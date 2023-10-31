375be2370fb835b4b42059f8460b71317fdd613b
deforum <deforum.art@gmail.com>
2023-10-30 21:29:11 -0700

    fuck up model lodading

---
+++ src/deforum/utils/model_download.py
@@ -0,0 +1,73 @@
+import os
+import requests
+from tqdm import tqdm
+from urllib.parse import urlparse
+
+
+def get_filename_from_url(url: str) -> str:
+    parsed_url = urlparse(url)
+    filename = os.path.basename(parsed_url.path)
+    return filename
+
+
+def download_file(
+    download_url: str, destination: str, filename: str, force_download: bool = False
+):
+    # Ensure download url is a string
+    assert isinstance(download_url, str), "Download URL should be a string."
+
+    # Ensure destination exists
+    os.makedirs(destination, exist_ok=True)
+
+    # Construct the full file path
+    filepath = os.path.join(destination, filename)
+
+    # Check if the file already exists
+    if os.path.exists(filepath):
+        if not force_download:
+            print(
+                f"File {filename} already exists at {destination}. Use force_download=True to re-download."
+            )
+            return filename
+        else:
+            print(
+                f"File {filename} already exists at {destination}. Re-downloading as per request."
+            )
+
+    # Download the file with a progress bar
+    print(f"Downloading {filename}...")
+    response = requests.get(download_url, stream=True)
+    total_size = int(response.headers.get("content-length", 0))
+    block_size = 1024  # 1 Kibibyte
+    t = tqdm(total=total_size, unit="iB", unit_scale=True)
+    with open(filepath, "wb") as file:
+        for data in response.iter_content(block_size):
+            t.update(len(data))
+            file.write(data)
+    t.close()
+
+    # Check for possible download errors
+    if total_size != 0 and t.n != total_size:
+        print("ERROR: Something went wrong while downloading the file.")
+    else:
+        print(f"{filename} downloaded successfully!")
+
+    return filename
+
+
+def download_from_civitai(
+    model_id: str, destination: str, force_download: bool = False
+):
+    # Ensure model id is a string
+    assert isinstance(model_id, str), "Model ID should be a string."
+
+    # Fetch the model details
+    response = requests.get(f"https://civitai.com/api/v1/models/{model_id}")
+    response.raise_for_status()
+
+    model_data = response.json()
+    download_url = model_data["modelVersions"][0]["downloadUrl"]
+    filename = model_data["modelVersions"][0]["files"][0]["name"]
+
+    # Use the helper function to download the model
+    return download_file(download_url, destination, filename, force_download)
---
+++ tests/test.py
@@ -0,0 +1,3 @@
+from deforum import DeforumAnimationPipeline
+
+deforum = DeforumAnimationPipeline.from_url(url="https://huggingface.co/Lykon/AbsoluteReality/resolve/main/AbsoluteRealityV1.6525-INPAINTING.inpainting.safetensors")
