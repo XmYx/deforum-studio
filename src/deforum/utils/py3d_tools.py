90d399ce1d5f3722eeec2bf2cc9430ed8a388b55
Miklos Nagy <miklos.mnagy@gmail.com>
2024-02-15 10:02:39 +0100

    Update py3d_tools.py

--- src/deforum/utils/py3d_tools.py
+++ src/deforum/utils/py3d_tools.py
@@ -1678,7 +1678,7 @@ def format_tensor(
     if not torch.is_tensor(input):
         input = torch.tensor(input, dtype=dtype, device=device_)
     elif not input.device.type.startswith('mps'):
-        input = torch.tensor(input, dtype=torch.float32, device=device_)
+        input = torch.tensor(input.clone().detach(), dtype=torch.float32, device=device_)
 
     if input.dim() == 0:
         input = input.view(1)
