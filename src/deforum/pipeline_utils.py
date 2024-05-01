c938e72c6c47feb47558d414508e99cd1cf40030
Miklos Nagy <miklos.mnagy@gmail.com>
2024-05-01 20:27:53 +0200

    output folder timestringed

--- src/deforum/pipeline_utils.py
+++ src/deforum/pipeline_utils.py
@@ -195,7 +195,7 @@ class DeforumGenerationObject(DeforumDataObject):
         self.raw_batch_name = self.batch_name
         # self.batch_name = substitute_placeholders(deforum.args.batch_name, current_arg_list,
         #                                                  full_base_folder_path)
-        self.outdir = os.path.join(full_base_folder_path, str(self.batch_name))
+        self.outdir = os.path.join(full_base_folder_path, f"{self.batch_name}_{self.timestring}")
         os.makedirs(self.outdir, exist_ok=True)
 
         # Handle seed initialization
